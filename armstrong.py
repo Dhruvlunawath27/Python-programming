1d781ae2beac42aad36a67a50ec6f89ca77e2f87
Dhruvlunawath27 <dhruv.lunawath@kore.com>
2025-04-16 19:29:04 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -5,3 +5,4 @@ def is_armstrong(num):
 
 def armstrong_numbers_in_range(start, end):
     return [n for n in range(start, end + 1) if is_armstrong(n)]
+    return num == sum(int(d)**power for d in digits)
